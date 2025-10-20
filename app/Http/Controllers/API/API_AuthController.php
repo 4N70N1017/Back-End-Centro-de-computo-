b7e2730a787c07b0f2064b891ae9e7d4befd4b19
Leoncio Martinez <leonciodanielmartinez@gmail.com>
2025-10-20 03:39:14 -0700

    API: Ruta iniciar sesion

--- app/Http/Controllers/API/API_AuthController.php
+++ app/Http/Controllers/API/API_AuthController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Usuario;
 
 class API_AuthController extends Controller
 {
-    //
+    public function iniciar_sesion(Request $request)
+    {
+        $request->validate([
+            'correo' => 'required|email',
+            'contrasena' => 'required',
+        ]);
+
+        $usuario = Usuario::where('correo', $request->correo)->first();
+
+        if (!$usuario || !Hash::check($request->contrasena, $usuario->contrasena)) {
+            return response()->json([
+                'ok' => false,
+                'mensaje' => 'Credenciales incorrectas.',
+            ], 401);
+        }
+
+        if (!$usuario->esta_activo) {
+            return response()->json([
+                'ok' => false,
+                'mensaje' => 'El usuario está inactivo.',
+            ], 403);
+        }
+
+        // Generar token con Sanctum
+        $token = $usuario->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'ok' => true,
+            'token' => $token,
+            /*
+            'usuario' => [
+                'id' => $usuario->id,
+                'correo' => $usuario->correo,
+                'rol' => $usuario->rol->nombre ?? null,
+                'empleado' => $usuario->empleado,
+            ],*/
+        ],200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\API_UsuarioController;
+use App\Http\Controllers\Api\API_AuthController;
+
 
 Route::post('/usuarios/guardar',[API_UsuarioController::class, 'guardar_usuario']);
+
+Route::post('/auth/inicar-sesion',[API_AuthController::class, 'iniciar_sesion']);
+
+//TODO: Falta poner el middleware auth:sanctum
